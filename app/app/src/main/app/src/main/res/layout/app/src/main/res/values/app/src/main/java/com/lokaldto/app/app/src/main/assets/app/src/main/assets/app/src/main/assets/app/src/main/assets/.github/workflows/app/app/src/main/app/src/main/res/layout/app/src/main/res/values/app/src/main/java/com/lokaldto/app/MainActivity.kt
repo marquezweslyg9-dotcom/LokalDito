b012ee75a7e0d0a20f271b17bd64daaf8fe08920
marquezweslyg9-dotcom <marquez.weslyg9@gmail.com>
2026-09-22 00:29:48 +0800

    Create MainActivity.kt

---
+++ app/app/src/main/app/src/main/res/layout/app/src/main/res/values/app/src/main/java/com/lokaldto/app/app/src/main/assets/app/src/main/assets/app/src/main/assets/app/src/main/assets/.github/workflows/app/app/src/main/app/src/main/res/layout/app/src/main/res/values/app/src/main/java/com/lokaldto/app/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.lokaldto.app
+
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+
+    @Suppress("DEPRECATION")
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}

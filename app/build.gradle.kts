fd0efd6d32539018050c9477a7de094780dc8816
marquezweslyg9-dotcom <marquez.weslyg9@gmail.com>
2026-09-22 18:38:56 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,29 @@
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.lokaldto.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.lokaldto.app"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 6
+        versionName = "1.5"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.webkit:webkit:1.12.1")
 }
